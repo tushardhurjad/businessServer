1fff9f714e22092cd2dacf5258f6d66d173f960d
tushar dhurjad <tushard2233@gmail.com>
2023-05-01 15:08:31 +0530

    Initial Commit

---
+++ innovation/src/main/java/com/dhurjadstech/innovation/InnovationApplication.java
@@ -0,0 +1,13 @@
+package com.dhurjadstech.innovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovationApplication.class, args);
+	}
+
+}
---
+++ innovation/src/test/java/com/dhurjadstech/innovation/InnovationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhurjadstech.innovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
